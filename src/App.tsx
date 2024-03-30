76080c28cc2bb3da2f9402cb717fbb239314f193
Jonthan Darko Adoo <adoojonathan412@gmail.com>
2024-03-30 14:15:48 +0000

    add Home component and add flex layout to components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import RightSideBar from './components/RightSideBar';
 import LeftSideBar from './components/LeftSideBar';
+import Home from './components/Home';
 
 const App: React.FC = () => {
   return (
-    <div>
-      <LeftSideBar />
-      <RightSideBar />
+    <div className="flex">
+      <div className="w-1/4">
+        <LeftSideBar />
+      </div>
+      <div className="w-1/2">
+        <Home />
+      </div>
+      <div className="w-1/4">
+        <RightSideBar />
+      </div>
     </div>
   );
 }
