bda6c1c45541e02afdeac6f0bc2862688db13540
Jonthan Darko Adoo <adoojonathan412@gmail.com>
2024-04-01 16:23:35 +0000

    successful fetching of popular moves from api

--- src/components/RightSideBar.tsx
+++ src/components/RightSideBar.tsx
@@ -1,74 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 const RightSideBar: React.FC = () => {
+   const [movies, setMovies] = useState([]);
+
+   useEffect(() => {
+      fetch('https://api.themoviedb.org/3/movie/popular?api_key=8734c4766281aca839b34feac6c89390')
+         .then(response => response.json())
+         .then(data => setMovies(data.results));
+   }, []);
+
    return (
-      <div className="right-sidebar">
+      <div className="right-sidebar mx-12">
          <div className="search-bar flex items-center rounded-full bg-white mt-4 mb-8">
             <FontAwesomeIcon icon={faSearch} className="text-gray-500 mr-4" />
-            <input type="text" placeholder="Search" className="pl-8 w-full py-2 rounded-full border border-gray-300" />
+            <input type="text" placeholder="Search" className="pl-8 w-full py-1 rounded-full border border-gray-300" />
          </div>
-         <div className="menu-header">
+         <div className="popular-movies">
             <h2 className="text-lg font-bold">Popular Movies</h2>
             <div className="grid grid-cols-1 gap-2 mt-4">
-               <div className="flex items-center mb-2">
+               {movies.slice(0, 3).map((movie: any) => (
+                  <div key={movie.id} className="flex">
                      <img
-                        src="path_to_image.jpg"
-                        alt="avengers"
-                        className="w-32 h-auto"
+                        src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                        alt={movie.title}
+                        className="w-16 h-auto rounded-lg"
                      />
-                     <p className="text-gray-600">Avengers</p>
+                     <p className="font-bold text-sm ml-4">{movie.title}</p>
                   </div>
-                  <div className="flex items-center mb-2">
-                     <img
-                        src="path_to_image.jpg"
-                        alt="mad max"
-                        className="w-32 h-auto"
-                     />
-                     <p className="text-gray-600">Mad Max</p>
-                  </div>
-                  <div className="flex items-center mb-2">
-                     <img
-                        src="path_to_image.jpg"
-                        alt="john wick 3"
-                        className="w-32 h-auto"
-                     />
-                     <p className="text-gray-600">John Wick 3</p>
-                  </div>
-                  <button className="bg-red-500 text-white py-2 px-4 rounded-lg mt-2">See More</button>
-               </div>
-         </div>
-         <div className="social-header mt-8">
-            <h2 className="text-lg font-bold">Favorites</h2>
-            <div className="grid grid-cols-1 gap-4 mt-2">
-               <div className="flex items-center mb-2">
-                     <img
-                        src="path_to_image.jpg"
-                        alt="avengers"
-                        className="w-32 h-auto"
-                     />
-                     <p className="text-gray-600">Avengers</p>
-                  </div>
-                  <div className="flex items-center mb-2">
-                     <img
-                        src="path_to_image.jpg"
-                        alt="mad max"
-                        className="w-32 h-auto"
-                     />
-                     <p className="text-gray-600">Mad Max</p>
-                  </div>
-                  <div className="flex items-center mb-2">
-                     <img
-                        src="path_to_image.jpg"
-                        alt="john wick 3"
-                        className="w-32 h-auto"
-                     />
-                     <p className="text-gray-600">John Wick 3</p>
-                  </div>
-                  <button className="bg-red-500 text-white py-2 px-4 rounded-lg mt-2">See More</button>
-               </div>
+               ))}
             </div>
+            <button className="bg-red-500 text-white py-2 px-4 rounded-lg mt-2 w-full">See More</button>
+         </div>
       </div>
    );
 };
