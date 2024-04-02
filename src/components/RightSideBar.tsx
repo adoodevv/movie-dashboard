92f92f3fb8557c923774d0dced284dd1511eadeb
Jonthan Darko Adoo <adoojonathan412@gmail.com>
2024-04-02 23:02:25 +0000

    feature updates

--- src/components/RightSideBar.tsx
+++ src/components/RightSideBar.tsx
@@ -17,6 +17,7 @@ interface Genre {
 const RightSideBar: React.FC = () => {
    const [movies, setMovies] = useState<Movie[]>([]);
    const [genres, setGenres] = useState<Genre[]>([]);
+   const [favorites, setFavorites] = useState<Movie[]>([]); // New state variable for favorite movies
 
    useEffect(() => {
       fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=8734c4766281aca839b34feac6c89390')
@@ -28,6 +29,12 @@ const RightSideBar: React.FC = () => {
       fetch('https://api.themoviedb.org/3/movie/popular?api_key=8734c4766281aca839b34feac6c89390')
          .then(response => response.json())
          .then(data => setMovies(data.results));
+   }, []);
+
+   useEffect(() => {
+      fetch('https://api.themoviedb.org/3/movie/top_rated?api_key=8734c4766281aca839b34feac6c89390') // Fetch favorite movies
+         .then(response => response.json())
+         .then(data => setFavorites(data.results));
    }, []);
 
    const getGenres = (genreIds: number[]) => {
@@ -60,8 +67,27 @@ const RightSideBar: React.FC = () => {
                   </div>
                ))}
             </div>
+            <button className="bg-red-600 text-white py-2 px-4 rounded-lg mt-4 mb-8 w-full">See More</button>
          </div>
-         <button className="bg-red-600 text-white py-2 px-4 rounded-lg mt-2 mb-8 w-full">See More</button>
+         <div className="top-rated-movies">
+            <h2 className="text-lg font-bold">Top Rated Movies</h2>
+            <div className="grid grid-cols-1 gap-2 mt-4">
+               {favorites.slice(0, 3).map((movie: Movie) => (
+                  <div key={movie.id} className="flex">
+                     <img
+                        src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                        alt={movie.title}
+                        className="w-16 h-auto rounded-lg"
+                     />
+                     <div>
+                        <p className="font-bold text-sm ml-4">{movie.title}</p>
+                        <p className="text-xs text-gray-400 ml-4">{getGenres(movie.genre_ids)}</p>
+                     </div>
+                  </div>
+               ))}
+            </div>
+            <button className="bg-red-600 text-white py-2 px-4 rounded-lg mt-4 mb-8 w-full">See More</button>
+         </div>
       </div>
    );
 };
