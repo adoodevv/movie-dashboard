746590a5bd61cf4ba08787152a24375d6645b14c
Jonthan Darko Adoo <adoojonathan412@gmail.com>
2024-04-02 23:00:06 +0000

    fetch upcoming moviees and add featured movie

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,13 +1,54 @@
 import React, { useEffect, useState } from 'react';
 
+interface Movie {
+   id: number;
+   title: string;
+   poster_path: string;
+   backdrop_path: string;
+   genre_ids: number[];
+   vote_average: number;
+}
+
+interface Genre {
+   id: number;
+   name: string;
+}
+
 const Home: React.FC = () => {
-   const [movies, setMovies] = useState([]);
+   const [movies, setMovies] = useState<Movie[]>([]);
+   const [featuredMovie, setFeaturedMovie] = useState<Movie | null>(null);
+   const [genres, setGenres] = useState<Genre[]>([]);
+   const [nowPlaying, setNowPlaying] = useState<Movie[]>([]);
 
    useEffect(() => {
-      fetch('https://api.themoviedb.org/3/movie/popular?api_key=8734c4766281aca839b34feac6c89390')
+      fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=8734c4766281aca839b34feac6c89390')
          .then(response => response.json())
-         .then(data => setMovies(data.results));
+         .then(data => setGenres(data.genres));
    }, []);
+
+   useEffect(() => {
+      fetch('https://api.themoviedb.org/3/movie/upcoming?api_key=8734c4766281aca839b34feac6c89390')
+         .then(response => response.json())
+         .then(data => {
+            setMovies(data.results);
+            setFeaturedMovie(data.results[2]);
+         });
+   }, []);
+
+   useEffect(() => {
+      fetch('https://api.themoviedb.org/3/movie/now_playing?api_key=8734c4766281aca839b34feac6c89390')
+         .then(response => response.json())
+         .then(data => {
+            setNowPlaying(data.results);
+         });
+   }, []);
+
+   const getGenres = (genreIds: number[]) => {
+      return genreIds.map(id => {
+         const genre = genres.find(g => g.id === id);
+         return genre ? genre.name : '';
+      }).join(', ');
+   };
 
    return (
       <div className="container mx-auto">
@@ -20,15 +61,48 @@ const Home: React.FC = () => {
                </ul>
             </nav>
          </header>
-         <div className="mt-8">
-            <h3 className="text-xl font-bold">Popular Movies</h3>
+         {featuredMovie && (
+            <div className="relative">
+               <img
+                  src={`https://image.tmdb.org/t/p/w1280${featuredMovie.backdrop_path}`} // Use backdrop_path instead of poster_path
+                  alt={featuredMovie.title}
+                  className="w-full h-auto rounded-3xl"
+               />
+               <div className="absolute bottom-0 left-0 p-4">
+                  <h2 className="text-4xl font-bold text-white">{featuredMovie.title}</h2>
+                  <div className="flex mt-2">
+                     <p className="text-l text-gray-200">{getGenres(featuredMovie.genre_ids).toUpperCase()}</p>
+                     <p className="text-white ml-4">%{featuredMovie.vote_average * 10} Match</p>
+                  </div>
+                  <button className="bg-red-600 text-white py-2 px-4 rounded-lg mt-4">Watch</button>
+                  <button className="bg-gray-600 bg-opacity-50 text-white py-2 px-4 rounded-lg mt-4 ml-4">+</button>
+               </div>
+            </div>
+         )}
+         <div className="upcoming-movies mt-8">
+            <h3 className="text-l text-gray-600 font-bold">Upcoming Movies</h3>
             <div className="grid grid-cols-4 gap-4 mt-4">
-               {movies.slice(0, 4).map((movie: any) => (
+               {movies.slice(3, 7).map((movie: Movie) => (
                   <div key={movie.id}>
                      <img
                         src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
                         alt={movie.title}
-                        className="w-32 h-auto"
+                        className="w-32 h-auto rounded-lg"
+                     />
+                     <p className="text-gray-600">{movie.title}</p>
+                  </div>
+               ))}
+            </div>
+         </div>
+         <div className="now-playing">
+            <h3 className="text-l text-gray-600 font-bold">Now Playing</h3>
+            <div className="grid grid-cols-4 gap-4 mt-4">
+               {nowPlaying.slice(0, 4).map((movie: Movie) => (
+                  <div key={movie.id}>
+                     <img
+                        src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                        alt={movie.title}
+                        className="w-32 h-auto rounded-lg"
                      />
                      <p className="text-gray-600">{movie.title}</p>
                   </div>
