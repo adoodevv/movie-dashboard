8b803a4acea6f87b5f96f63d655d9ae6ba3e5019
Jonthan Darko Adoo <adoojonathan412@gmail.com>
2024-04-05 09:58:38 +0000

    add hover effect to home buttons

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -64,23 +64,32 @@ const Home: React.FC = () => {
          {featuredMovie && (
             <div className="relative">
                <img
-                  src={`https://image.tmdb.org/t/p/w1280${featuredMovie.backdrop_path}`} // Use backdrop_path instead of poster_path
+                  src={`https://image.tmdb.org/t/p/w1280${featuredMovie.backdrop_path}`}
                   alt={featuredMovie.title}
                   className="w-full h-auto rounded-3xl"
                />
+               <div className="absolute top-0 right-0 text-white p-4">
+                  <button className="bg-gray-600 bg-opacity-50 text-white px-4 py-2 rounded-full items-center hover:bg-gray-700">&gt;</button>
+               </div>
                <div className="absolute bottom-0 left-0 p-4">
                   <h2 className="text-4xl font-bold text-white">{featuredMovie.title}</h2>
                   <div className="flex mt-2">
                      <p className="text-l text-gray-200">{getGenres(featuredMovie.genre_ids).toUpperCase()}</p>
-                     <p className="text-white ml-4">%{featuredMovie.vote_average * 10} Match</p>
+                     <p className="text-white ml-4">%{Math.ceil(featuredMovie.vote_average * 10)} Match</p>
                   </div>
                   <button className="bg-red-600 text-white py-2 px-4 rounded-lg mt-4">Watch</button>
                   <button className="bg-gray-600 bg-opacity-50 text-white py-2 px-4 rounded-lg mt-4 ml-4">+</button>
                </div>
             </div>
          )}
-         <div className="upcoming-movies mt-8">
-            <h3 className="text-l text-gray-600 font-bold">Upcoming Movies</h3>
+         <div className="upcoming-movies mt-8 mb-8">
+            <div className="flex justify-between items-center">
+               <h3 className="text-l text-gray-600 font-bold">Upcoming Movies</h3>
+               <div className="buttons">
+                  <button className="border-2 border-gray-600 text-gray-600 px-2 rounded-full items-center hover:bg-gray-200">&lt;</button>
+                  <button className="border-2 border-gray-600 text-gray-600 px-2 rounded-full items-center hover:bg-gray-200 ml-4">&gt;</button>
+               </div>
+            </div>
             <div className="grid grid-cols-4 gap-4 mt-4">
                {movies.slice(3, 7).map((movie: Movie) => (
                   <div key={movie.id}>
@@ -95,7 +104,13 @@ const Home: React.FC = () => {
             </div>
          </div>
          <div className="now-playing">
-            <h3 className="text-l text-gray-600 font-bold">Now Playing</h3>
+            <div className="flex justify-between items-center">
+               <h3 className="text-l text-gray-600 font-bold">Now Playing</h3>
+               <div className="buttons">
+                  <button className="border-2 border-gray-600 text-gray-600 px-2 rounded-full items-center hover:bg-gray-200">&lt;</button>
+                  <button className="border-2 border-gray-600 text-gray-600 px-2 rounded-full items-center hover:bg-gray-200 ml-4">&gt;</button>
+               </div>
+            </div>
             <div className="grid grid-cols-4 gap-4 mt-4">
                {nowPlaying.slice(0, 4).map((movie: Movie) => (
                   <div key={movie.id}>
