4fc53f6daefed7cc655dd0ac39efdc7bbb8db5a5
Jonathan Darko Adoo <adoojonathan412@gmail.com>
2024-04-07 18:17:34 +0000

    refactor Home component for neater and readable code

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAngleLeft, faAngleRight, faPlus } from '@fortawesome/free-solid-svg-icons';
+import { faAngleRight, faPlus } from '@fortawesome/free-solid-svg-icons';
+import UpcomingMovies from './UpcomingMovies';
+import NowPlaying from './NowPlaying';
 
 interface Movie {
    id: number;
@@ -17,10 +19,8 @@ interface Genre {
 }
 
 const Home: React.FC = () => {
-   const [movies, setMovies] = useState<Movie[]>([]);
    const [featuredMovie, setFeaturedMovie] = useState<Movie | null>(null);
    const [genres, setGenres] = useState<Genre[]>([]);
-   const [nowPlaying, setNowPlaying] = useState<Movie[]>([]);
 
    useEffect(() => {
       fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=8734c4766281aca839b34feac6c89390')
@@ -32,16 +32,7 @@ const Home: React.FC = () => {
       fetch('https://api.themoviedb.org/3/movie/upcoming?api_key=8734c4766281aca839b34feac6c89390')
          .then(response => response.json())
          .then(data => {
-            setMovies(data.results);
             setFeaturedMovie(data.results[2]);
-         });
-   }, []);
-
-   useEffect(() => {
-      fetch('https://api.themoviedb.org/3/movie/now_playing?api_key=8734c4766281aca839b34feac6c89390')
-         .then(response => response.json())
-         .then(data => {
-            setNowPlaying(data.results);
          });
    }, []);
 
@@ -88,56 +79,8 @@ const Home: React.FC = () => {
                </div>
             </div>
          )}
-         <div className="upcoming-movies mt-8 mb-8">
-            <div className="flex justify-between items-center">
-               <h3 className="text-l text-gray-600 font-bold">Upcoming Movies</h3>
-               <div className="buttons">
-                  <button className="border-2 border-gray-600 text-gray-600 px-2 rounded-full items-center hover:bg-gray-200">
-                     <FontAwesomeIcon icon={faAngleLeft} className="text-gray-600" />
-                  </button>
-                  <button className="border-2 border-gray-600 text-gray-600 px-2 rounded-full items-center hover:bg-gray-200 ml-4">
-                     <FontAwesomeIcon icon={faAngleRight} className="text-gray-600" />
-                  </button>
-               </div>
-            </div>
-            <div className="grid grid-cols-4 gap-4 mt-4">
-               {movies.slice(3, 7).map((movie: Movie) => (
-                  <div key={movie.id}>
-                     <img
-                        src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-                        alt={movie.title}
-                        className="w-32 h-auto rounded-lg"
-                     />
-                     <p className="text-gray-600">{movie.title}</p>
-                  </div>
-               ))}
-            </div>
-         </div>
-         <div className="now-playing">
-            <div className="flex justify-between items-center">
-               <h3 className="text-l text-gray-600 font-bold">Now Playing</h3>
-               <div className="buttons">
-                  <button className="border-2 border-gray-600 text-gray-600 px-2 rounded-full items-center hover:bg-gray-200">
-                     <FontAwesomeIcon icon={faAngleLeft} className="text-gray-600" />
-                  </button>
-                  <button className="border-2 border-gray-600 text-gray-600 px-2 rounded-full items-center hover:bg-gray-200 ml-4">
-                     <FontAwesomeIcon icon={faAngleRight} className="text-gray-600" />
-                  </button>
-               </div>
-            </div>
-            <div className="grid grid-cols-4 gap-4 mt-4">
-               {nowPlaying.slice(0, 4).map((movie: Movie) => (
-                  <div key={movie.id}>
-                     <img
-                        src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-                        alt={movie.title}
-                        className="w-32 h-auto rounded-lg"
-                     />
-                     <p className="text-gray-600">{movie.title}</p>
-                  </div>
-               ))}
-            </div>
-         </div>
+         <UpcomingMovies />
+         <NowPlaying />
       </div>
    );
 };
