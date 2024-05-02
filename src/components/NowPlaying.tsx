c82cd150a8f54f651df9f54dc2c122536b260dd5
Jonthan Darko Adoo <adoojonathan412@gmail.com>
2024-05-02 03:40:10 +0000

    forward and backward button functionality added

--- src/components/NowPlaying.tsx
+++ src/components/NowPlaying.tsx
@@ -14,6 +14,16 @@ interface Movie {
 const NowPlaying: React.FC = () => {
    const [nowPlaying, setNowPlaying] = useState<Movie[]>([]);
 
+   const [start, setStart] = useState(0);
+
+   const handleForward = () => {
+      setStart(prevStart => prevStart + 2);
+   };
+
+   const handleBack = () => {
+      setStart(prevStart => Math.max(prevStart - 2, 0));
+   };
+
    useEffect(() => {
       fetch('https://api.themoviedb.org/3/movie/now_playing?api_key=8734c4766281aca839b34feac6c89390')
          .then(response => response.json())
@@ -23,20 +33,20 @@ const NowPlaying: React.FC = () => {
    }, []);
 
    return (
-         <div className="now-playing">
+         <div className="now-playing mb-4">
             <div className="flex justify-between items-center">
                <h3 className="text-l text-gray-600 font-bold">Now Playing</h3>
                <div className="buttons">
-                  <button className="border-2 border-gray-600 text-gray-600 px-2 rounded-full items-center hover:bg-gray-200">
+                  <button onClick={handleBack} className="border-2 border-gray-600 text-gray-600 px-2 rounded-full items-center hover:bg-gray-200">
                      <FontAwesomeIcon icon={faAngleLeft} className="text-gray-600" />
                   </button>
-                  <button className="border-2 border-gray-600 text-gray-600 px-2 rounded-full items-center hover:bg-gray-200 ml-4">
+                  <button onClick={handleForward} className="border-2 border-gray-600 text-gray-600 px-2 rounded-full items-center hover:bg-gray-200 ml-4">
                      <FontAwesomeIcon icon={faAngleRight} className="text-gray-600" />
                   </button>
                </div>
             </div>
             <div className="grid grid-cols-4 gap-4 mt-4">
-               {nowPlaying.slice(0, 4).map((movie: Movie) => (
+               {nowPlaying.slice(start, start + 4).map((movie: Movie) => (
                   <div key={movie.id}>
                      <img
                         src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
